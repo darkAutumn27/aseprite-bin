218672fae08786b5d367b76852c9be587b1d1e40
darkAutumn27 <Jaundreschacht@gmail.com>
2025-09-01 14:46:42 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -40,7 +40,7 @@ where /q ninja.exe || (
 
 rem *** latest Aseprite version ***
 
-set ASEPRITE_VERSION=v1.3.14.4
+set ASEPRITE_VERSION=v1.3.15.2
 set SKIA_VERSION=m124-08a5439a6b
 
 echo Building %ASEPRITE_VERSION%
